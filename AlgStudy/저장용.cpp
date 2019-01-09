35d3e2e23975e0c81f799c6ad95f4ed1efcc94ec
98765kjh <98765kjh@gmail.com>
2019-01-09 11:08:36 +0900

    before merge

--- AlgStudy/저장용.cpp
+++ AlgStudy/저장용.cpp
@@ -1,28 +1,27 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<cstdio>
-//#include<vector>
-//#include<string>
-//#include<cstring>
-//#include<iostream>
-//
-//using namespace std;
-//int main() {
-//	string input, temp;
-//	cin >> input;
-//	int cnt = 0;
-//	for (int i = 0; i < input.size(); i++) {
-//		temp = input.substr(i, input.size() - i);
-//		bool flag = true;
-//		for (int j = 0; j < temp.size() / 2; j++) {
-//			if (temp[j] != temp[temp.size() - 1 - j]) {
-//				flag = false;
-//				break;
-//			}
-//		}
-//		if (flag) {
-//			cnt = i;
-//			break;
-//		}
-//	}
-//	printf("%d\n", temp.size() + cnt * 2);
-//}
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+	bitset<8> bs;
+
+	hash<bitset<8>> hash_fun;
+
+	cout << bs << endl;
+
+	bs |= 5;
+	cout << bs << endl;
+	cout << bs.count() << endl;
+	cout << bs.flip() << endl;
+	cout << bs << endl;
+
+	
+	cout << bs << endl;
+
+	cout << bs.size() << endl;
+
+	cout << hash_fun(bs) << endl;
+
+	return 0;
+}
